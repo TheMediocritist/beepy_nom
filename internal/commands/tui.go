0f959ac7e8c9b81e0683e6e3296894115006aa8f
TheMediocritist <79881777+TheMediocritist@users.noreply.github.com>
2023-07-08 22:27:16 +1000

    Update tui.go

--- internal/commands/tui.go
+++ internal/commands/tui.go
@@ -21,15 +21,14 @@ var (
 	appStyle          = lipgloss.NewStyle().Padding(0).Margin(0).Background(lipgloss.Color("#ffffff"))
 	titleStyle        = list.DefaultStyles().Title.Margin(1, 0, 0, 0)
 	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
-	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("#000000"))
 	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
 	helpStyle         = list.DefaultStyles().
 				HelpStyle.
 				PaddingLeft(0).
 				PaddingBottom(0).
+				Background(lipgloss.Color("#000000").
 				Foreground(lipgloss.Color("#ffffff"))
-	
-				Background(lipgloss.Color("#000000"))
 )
 
 type Item struct {
